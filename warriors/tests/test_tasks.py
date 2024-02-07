96c201e289fe10170b89bf121f4e95150ae2b08c
Jan Rydzewski <flegmer@gmail.com>
2024-02-07 22:31:11 +0100

    Test do_moderation

--- warriors/tests/test_tasks.py
+++ warriors/tests/test_tasks.py
@@ -1,9 +1,36 @@
+from unittest import mock
+
 import pytest
 from django.utils import timezone
 
 from ..models import Battle, Warrior
-from ..tasks import schedule_battles
+from ..tasks import do_moderation, openai_client, schedule_battles
 from .factories import WarriorFactory
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('warrior', [{
+    'name': 'Test Warrior',
+    'author': 'Test Author',
+}], indirect=True)
+def test_do_moderation(warrior, monkeypatch):
+    assert warrior.body
+    assert warrior.name
+    assert warrior.author
+
+    moderation_result_mock = mock.MagicMock()
+    moderation_result_mock.flagged = True
+    moderation_mock = mock.MagicMock()
+    moderation_mock.return_value.model = 'mderation-asdf'
+    moderation_mock.return_value.results = [moderation_result_mock]
+    monkeypatch.setattr(openai_client.moderations, 'create', moderation_mock)
+
+    do_moderation(warrior.id)
+
+    warrior.refresh_from_db()
+    assert warrior.moderation_date is not None
+    assert warrior.moderation_flagged is True
+    assert warrior.moderation_model
 
 
 @pytest.mark.django_db
