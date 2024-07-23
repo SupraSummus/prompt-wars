12c90a0f87104b86761eaf55009774a6775dce0c
Jan Rydzewski <SupraSummus@users.noreply.github.com>
2024-07-23 17:43:07 +0200

    Public battles (#12)
    * Added a boolean field to indicate whether battle results should be public for a user-warrior.
    
    * Added a boolean field `public_battle_results` to the Warrior model to indicate whether battle results should be public for a given warrior.
    
    * Added a new boolean field `public_battle_results` to the `WarriorUserPermission` model in `warriors/models.py` to control the visibility of battle results for each user-warrior combination.
    
    * Public battle results mechanics

--- warriors/models.py
+++ warriors/models.py
@@ -154,6 +154,11 @@ class Warrior(models.Model):
         default=0.0,
     )
 
+    public_battle_results = models.BooleanField(
+        default=False,
+        help_text=_("Indicates whether battle results should be public for this warrior."),
+    )
+
     @property
     def rating_error_abs(self):
         return abs(self.rating_error)
@@ -338,6 +343,18 @@ class Warrior(models.Model):
 
         return rating_error
 
+    def update_public_battle_results(self):
+        """Recompute public_battle_results based on per user data"""
+        user_permissions = list(WarriorUserPermission.objects.filter(
+            warrior=self,
+        ))
+        if user_permissions:
+            self.public_battle_results = any(
+                up.public_battle_results
+                for up in user_permissions
+            )
+            self.save(update_fields=['public_battle_results'])
+
     @cached_property
     def secret(self):
         return self.secret_signer.sign(str(self.id)).split(':')[1]
@@ -377,6 +394,9 @@ class WarriorUserPermission(models.Model):
     name = models.CharField(
         max_length=40,
         blank=True,
+    )
+    public_battle_results = models.BooleanField(
+        default=False,
     )
 
     class Meta:
@@ -594,6 +614,10 @@ class Battle(models.Model):
     @cached_property
     def game_2_1(self):
         return Game(self, '2_1')
+
+    @property
+    def public_battle_results(self):
+        return self.warrior_1.public_battle_results or self.warrior_2.public_battle_results
 
     def get_warrior_viewpoint(self, warrior):
         """Return Battle such that warrior_1 == warrior"""
