77ea4a6ae71d0cf771bd691ed099170d06b2b534
Jan Rydzewski <flegmer@gmail.com>
2024-09-26 15:59:19 +0200

    Remove unify warrior

--- warriors/cross_arena.py
+++ warriors/cross_arena.py
@@ -1,35 +1,9 @@
 import logging
 
 from .models import Arena, WarriorArena
-from .warriors import Warrior
 
 
 logger = logging.getLogger(__name__)
-
-
-def find_and_unify_warrior():
-    # find warrior-arena withut a warrior
-    logger.info('Doing find_and_unify_warrior')
-    warrior_arena = WarriorArena.objects.filter(
-        warrior=None,
-    ).order_by('id').first()
-    if not warrior_arena:
-        logger.info('No warrior-arena without warrior found')
-        return
-    warrior = get_or_create_warrior(warrior_arena)
-    logger.info(f'Unified warrior-arena {warrior_arena.id} with warrior {warrior.id}')
-
-
-def get_or_create_warrior(warrior_arena):
-    warrior, created = Warrior.objects.get_or_create(
-        body_sha_256=warrior_arena.body_sha_256,
-        defaults={
-            'body': warrior_arena.body,
-        },
-    )
-    meld_warrior(warrior, warrior_arena)
-    # ensure_warrior_on_all_arenas(warrior)
-    return warrior
 
 
 def ensure_warrior_on_all_arenas(warrior):
@@ -50,53 +24,3 @@ def ensure_warrior_on_all_arenas(warrior):
                 'public_battle_results': warrior.public_battle_results,
             },
         )
-        if not created:
-            meld_warrior(warrior, warrior_arena)
-
-
-def meld_warrior(warrior, warrior_arena):
-    assert warrior.body_sha_256 == warrior_arena.body_sha_256
-    modified = False
-
-    # assign authorship of the oldest instance
-    if warrior_arena.created_at < warrior.created_at:
-        warrior.created_at = warrior_arena.created_at
-        warrior.created_by = warrior_arena.created_by
-        warrior.name = warrior_arena.name
-        warrior.author_name = warrior_arena.author_name
-        modified = True
-
-    # assign most recent moderation data
-    if (
-        # global warrior not moderated or
-        (
-            warrior_arena.moderation_passed and
-            (warrior.moderation_passed is None)
-        ) or
-        # we have newer moderation data
-        (
-            warrior_arena.moderation_date and
-            warrior.moderation_date and
-            warrior_arena.moderation_date > warrior.moderation_date
-        )
-    ):
-        warrior.moderation_date = warrior_arena.moderation_date
-        warrior.moderation_passed = warrior_arena.moderation_passed
-        warrior.moderation_model = warrior_arena.moderation_model
-        modified = True
-
-    # assign public_battle_results
-    if (
-        warrior_arena.public_battle_results and
-        not warrior.public_battle_results
-    ):
-        warrior.public_battle_results = warrior_arena.public_battle_results
-        modified = True
-
-    if modified:
-        warrior.save()
-
-    # link warrior_arena to the warrior
-    if warrior_arena.warrior != warrior:
-        warrior_arena.warrior = warrior
-        warrior_arena.save(update_fields=['warrior'])
--- warriors/scheduler.py
+++ warriors/scheduler.py
@@ -1,7 +1,6 @@
 import logging
 import sched
 
-from .cross_arena import find_and_unify_warrior
 from .stats import create_arena_stats
 from .tasks import schedule_battle, schedule_battles_top, update_rating
 
@@ -29,4 +28,3 @@ scheduler.enter_recurring(1, 0, schedule_battle)
 scheduler.enter_recurring(60 * 10, 0, schedule_battles_top)
 scheduler.enter_recurring(60, 0, update_rating)
 scheduler.enter_recurring(60 * 60, 0, create_arena_stats)
-scheduler.enter_recurring(17, 0, find_and_unify_warrior)
