188f883ce937be6e97ad98ee5be212709b768146
Jan Rydzewski <flegmer@gmail.com>
2024-11-16 03:16:59 +0100

    Save memory

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 trap '' SIGTERM
 
-gunicorn llm_wars.wsgi &
-python manage.py goals_busy_worker &
+gunicorn llm_wars.wsgi --max-requests 100 --max-requests-jitter 10 &
+(yes | xargs -I -L1 -P1 -- python manage.py goals_busy_worker --max-progress-count 10) &
 python manage.py scheduler &
 
 wait -n
