9d11908fd9a0b1e988768e1128c08eeca953e5dd
Jan Rydzewski <flegmer@gmail.com>
2024-11-16 22:45:41 +0100

    Fix process management

--- run.sh
+++ run.sh
@@ -1,10 +1,19 @@
+# Heroku's dyno manager sends SIGTERM to all processes in the dyno (not just the root process)
+# We ignore it in root process
 trap '' SIGTERM
 
 gunicorn llm_wars.wsgi --max-requests 100 --max-requests-jitter 10 &
-(yes | xargs -I -L1 -P1 -- python manage.py goals_busy_worker --max-progress-count 10) &
+(
+    trap 'exit' SIGTERM
+    while true; do
+        python manage.py goals_busy_worker --max-progress-count 10
+    done
+) &
 python manage.py scheduler &
 
+# Wait for any child processes to exit
 wait -n
 
+# We send SIGTERM to all processes in the process group, not just the root process
 kill -SIGTERM -$$
 wait
