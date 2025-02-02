735bce4177f6cff6e62bb182cded75b94769b5f1
Jan Rydzewski <flegmer@gmail.com>
2025-02-02 13:36:45 +0100

    Scheduler wip

---
+++ django_scheduler/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoSchedulerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_scheduler'
---
+++ django_scheduler/models.py
@@ -0,0 +1,114 @@
+from django.db import models
+import uuid
+from dataclasses import dataclass
+from typing import Callable
+import datetime
+import logging
+from django.utils import timezone
+import time
+
+
+logger = logging.getLogger(__name__)
+
+
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    key = models.CharField(max_length=100, unique=True)
+    last_run = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['key']
+
+
+@dataclass(frozen=True)
+class LocalJob:
+    """
+    A job as defined in current process
+    """
+    key: str
+    handler: Callable
+    interval: datetime.timedelta
+
+
+_local_jobs = []
+
+
+def register_job(handler, interval):
+    job = LocalJob(key=handler.__name__, handler=handler, interval=interval)
+    _local_jobs.append(job)
+
+
+def run(local_jobs=None, blocking=True):
+    if local_jobs is None:
+        local_jobs = _local_jobs.copy()
+        del _local_jobs  # prevent adding more jobs after we started
+
+    # build a queue of jobs to run
+    db_jobs = get_or_create_db_jobs(local_jobs)
+    queue = []
+    now = timezone.now()
+    for local_job in local_jobs:
+        db_job = db_jobs[local_job.key]
+        if db_job.last_run:
+            next_run = db_job.last_run + local_job.interval
+        else:
+            next_run = now
+        queue.append((next_run, local_job))
+    queue.sort()
+    del db_jobs
+
+    # run jobs
+    while True:
+        now = timezone.now()
+        if not queue:
+            logger.info("No jobs in queue, exiting")
+            break
+
+        next_run, local_jon = queue[0]
+        if next_run > now:
+            if not blocking:
+                logger.info("Next job is in the future, exiting")
+                break
+            time_to_sleep = (next_run - now).total_seconds()
+            logger.info("Sleeping for %s seconds until next job %s", time_to_sleep, local_job.key)
+            time.sleep(time_to_sleep)
+
+        next_run, local_job = queue.pop(0)
+        db_job = run_job(local_job)
+
+        queue.append((next_run + local_job.interval, local_job))
+        queue.sort()
+
+
+def get_or_create_db_jobs(local_jobs):
+    db_jobs = {
+        db_job.key: db_job
+        for db_job in Job.objects.filter(
+            key__in=[local_job.key for local_job in local_jobs]
+        )
+    }
+    for local_job in local_jobs:
+        if local_job.key not in db_jobs:
+            logger.info("Creating missing DB job entry, key=%s", local_job.key)
+            db_job = Job(key=local_job.key)
+            db_job.save()
+            db_jobs[local_job.key] = db_job
+    return db_jobs
+
+
+@transaction.atomic
+def run_job(local_job):
+    db_job = Job.objects.filter(
+        key=local_job.key,
+    ).select_for_update(
+        no_key=True,
+    ).first()
+    if not db_job:
+        db_job = get_or_create_db_jobs([local_job])[local_job.key]
+
+    logger.info("Running job %s", local_job.key)
+    db_job.last_run = timezone.now()
+    try:
+        local_job.handler()
+    db_job.save(update_fields=['last_run'])
+    return db_job
