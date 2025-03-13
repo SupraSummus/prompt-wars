e7fcb1330cfc48e9c7ef886c69c1eed686aa5647
Jan Rydzewski <flegmer@gmail.com>
2025-03-13 17:17:39 +0100

    Run clearsession from time to time

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
+import datetime
 import uuid
 
 from django.contrib.auth.models import AbstractUser
+from django.core.management import call_command
 from django.db import models
+
+from django_scheduler.models import register_job
 
 
 class User(AbstractUser):
@@ -10,3 +14,10 @@ class User(AbstractUser):
         default=uuid.uuid4,
         editable=False,
     )
+
+
+register_job(
+    lambda now: call_command('clearsessions'),
+    interval=datetime.timedelta(days=1),
+    key='clearsessions',
+)
