b45bfc3efb8acc6d0bf3000c9427e5efc56847e3
Jan Rydzewski <flegmer@gmail.com>
2025-04-05 12:33:20 +0200

    Fix equal-cooperation metric

--- warriors/score.py
+++ warriors/score.py
@@ -242,5 +242,5 @@ class GameScoreViewpoint:
             self.warrior_2_similarity is None
         ):
             return None
-        similarity_diff = (self.warrior_1_similarity - self.warrior_2_similarity) ** 2
-        return similarity_diff * (1 - self.warriors_similarity)
+        similarity_diff = self.warrior_1_similarity - self.warrior_2_similarity
+        return (1 - similarity_diff ** 2) * (1 - self.warriors_similarity)
