774555d9c953ea57c7197c905a8bf5ac6ffc09ed
Jan Rydzewski <SupraSummus@users.noreply.github.com>
2025-10-09 22:00:18 +0200

    gemini-flash-lite-latest

--- warriors/llms/google.py
+++ warriors/llms/google.py
@@ -26,7 +26,7 @@ def resolve_battle_google(prompt_a, prompt_b, system_prompt=''):
 def call_gemini(prompt):
     try:
         response = client.models.generate_content(
-            model='gemini-2.5-flash-lite',
+            model='gemini-flash-lite-latest',
             contents=prompt,
             config=GenerateContentConfig(
                 temperature=0,
